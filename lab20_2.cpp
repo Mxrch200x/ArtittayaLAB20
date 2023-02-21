e74be9ac1ab31d5479f198ef4e5969ebd20fffa5
Mxrch200x <gaerdbigsis@gmail.com>
2023-02-21 16:07:51 +0700

    completed

--- lab20_2.cpp
+++ lab20_2.cpp
@@ -1,12 +1,27 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 
 struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect *R1, Rect *R2) {
+	double xx = abs(R1->x - R2->x);
+	double yy = abs(R1->y - R2->y);
+	double xxx, yyy;
+	if(xx > R1->w || yy > R1->h) return 0;
+	else{
+		if (xx < R1->w - R2->w){
+			xxx = R2->w;
+		}else{
+			xxx = R1->w - xx;
+		}
+		if (yy < R1->h - R2->h){
+			yyy = R2->h;
+		}else{
+			yyy = R1->h - yy;
+		}
+	}
+	return xxx *yyy;
 }
